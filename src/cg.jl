1fe3b2b627e361868e4d390ccc4492eec41ff834
Arpit Babbar <arpitbabbar@gmail.com>
2025-02-19 16:07:04 +0100

    Have the code support GPU through KernelAbstractions.jl (#1)
    * Make code work with GPU
    
    * Fix!
    
    * Make test backend independent
    
    * Fix n
    
    * Some improvements, CI
    
    * Add missing file
    
    * Add CI
    
    * Add Project.toml to test
    
    * Fix BICGSTAB and tests
    
    Retry CI
    
    Retry CI
    
    Another try for CI
    
    Fix CI

--- src/cg.jl
+++ src/cg.jl
@@ -1,21 +1,26 @@
+using KernelAbstractions
+
 # Data container
-struct CGData{T<:Real}
-    r::Vector{T}
-    z::Vector{T}
-    p::Vector{T}
-    Ap::Vector{T}
-    CGData(n::Int, T::Type) = new{T}(
-        zeros(T, n), zeros(T, n),
-        zeros(T, n), zeros(T, n))
+struct CGData{T <: Real, AbstractBackend <: KernelAbstractions.Backend}
+    r::AbstractVector{T}
+    z::AbstractVector{T}
+    p::AbstractVector{T}
+    Ap::AbstractVector{T}
+    backend::AbstractBackend
+    CGData(n::Int, T::Type, backend::KernelAbstractions.Backend) = new{T, typeof(backend)}(
+        KernelAbstractions.zeros(backend, eltype(T), n),
+        KernelAbstractions.zeros(backend, eltype(T), n),
+        KernelAbstractions.zeros(backend, eltype(T), n),
+        KernelAbstractions.zeros(backend, eltype(T), n))
 end
 
 # Solves for x
-function cg!(A, b::Vector{T}, x::Vector{T};
-             tol::Float64=1e-6, maxIter::Int64=100,
+function cg!(A, b::AbstractVector{T}, x::AbstractVector{T};
+             tol::T=map(T,1e-6), maxIter::Int=100,
              precon=copy!,
-             data=CGData(length(b), T)) where {T<:Real}
-    if genblas_nrm2(b) == 0.0
-        x .= 0.0
+             data=CGData(length(b), T, get_backend(b))) where {T<:Real}
+    if genblas_nrm2(b) == zero(T)
+        x .= zero(T)
         return 1, 0
     end
     A(data.r, x)
@@ -46,17 +51,18 @@ function cg!(A, b::Vector{T}, x::Vector{T};
         beta = genblas_dot(data.z, data.r)/gamma
         # p = z + beta*p
         genblas_scal!(beta, data.p)
-        genblas_axpy!(1.0, data.z, data.p)
+        genblas_axpy!(1.0f0, data.z, data.p)
     end
     return -2, maxIter
 end
 
 # API
-function cg(A, b::Vector{T};
-            tol::Float64=1e-6, maxIter::Int64=100,
+function cg(A, b::AbstractVector{T};
+            tol::T=map(T, 1e-6), maxIter::Int=100,
             precon=copy!,
-            data=CGData(length(b), T)) where {T<:Real}
-    x = zeros(eltype(b), length(b))
+            data=CGData(length(b), T, get_backend(b))) where {T<:Real}
+    backend = get_backend(b)
+    x = KernelAbstractions.zeros(backend, eltype(b), length(b))
     exit_code, num_iters = cg!(A, b, x, tol=tol, maxIter=maxIter, precon=precon, data=data)
     return x, exit_code, num_iters
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,4 @@
-using ConjugateGradients
-using LinearAlgebra
-using SparseArrays
-using Test
-
-function test_cg(n=100)
-    tA = sprandn(n, n, 0.1) + spdiagm(0=>fill(10.0, n))
-    A = tA'*tA
-    b = rand(n)
-    true_x = A\b
-    x, exit_code, num_iters = cg((x,y) -> mul!(x, A, y), b)
-    norm(true_x - x) < 1e-6
-end
-
-function test_bicgstab(n=100)
-    A = sprandn(n, n, 0.1) + spdiagm(0=>fill(10.0, n))
-    b = rand(n)
-    true_x = A\b
-    x, exit_code, num_iters = bicgstab((x,y) -> mul!(x, A, y), b)
-    norm(true_x - x) < 1e-6
-end
+include("test_base.jl")
 
 @testset "ConjugateGradients" begin
     @test test_cg()
